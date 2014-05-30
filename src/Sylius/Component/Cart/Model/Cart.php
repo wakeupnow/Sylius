5336a2241c461144774284b52e594374ac4042f6
Sergey <laa513@gmail.com>
2014-05-30 16:28:23 +0300

    changed total calculations to lifecycle callback

--- src/Sylius/Component/Cart/Model/Cart.php
+++ src/Sylius/Component/Cart/Model/Cart.php
@@ -137,4 +137,17 @@ class Cart extends Order implements CartInterface
 
         return $this;
     }
+
+    /**
+     * @return void
+     */
+    public function calculateTotals()
+    {die('ok');
+        $cartTotal = 0;
+        foreach ($this->getItems() as $item) {
+            $item->setTotal($item->getQuantity() * $item->getUnitPrice());
+            $cartTotal += $item->getTotal();
+        }
+        $this->setTotal($cartTotal);
+    }
 }
