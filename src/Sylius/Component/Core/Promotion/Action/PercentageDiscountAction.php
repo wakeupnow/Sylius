7327f07bb8711d4ca950e884dd9ffe80bb15276c
Sergey <laa513@gmail.com>
2014-06-11 20:59:13 +0300

    promotions hack

--- src/Sylius/Component/Core/Promotion/Action/PercentageDiscountAction.php
+++ src/Sylius/Component/Core/Promotion/Action/PercentageDiscountAction.php
@@ -46,6 +46,78 @@ class PercentageDiscountAction implements PromotionActionInterface
      */
     public function execute(PromotionSubjectInterface $subject, array $configuration, PromotionInterface $promotion)
     {
+        // Product promo -----------------------------------------------------------------------------------------------
+
+        // this is a proof of what we can apply needed adjustments,
+        // i used lambdas just to keep all this experiment code together
+
+        $adjustmentRepository = $this->repository;
+
+        // this determines whether a promotion is of this new type
+        $isProductPromotion = function (PromotionInterface $promotion) {
+            foreach ($promotion->getRules() as $rule) {
+                if ($rule->getType() == 'product') {
+                    return true;
+                }
+            }
+
+            return false;
+        };
+
+        // this compares Promotion with OrderItem, checks if OrderItem is eligible for this promotion and
+        // if it's eligible it returns an Adjustment representing discount
+        $getProductPromotionAdjustment = function(\Sylius\Component\Core\Model\OrderItemInterface $orderItem)
+                                          use ($adjustmentRepository, $configuration, $promotion)
+        {
+            foreach ($promotion->getRules() as $rule) {
+                if ($rule->getType() === 'product') {
+                    $config = $rule->getConfiguration();
+
+                    if ($config['products']->contains($orderItem->getProduct()->getId())
+                        && $orderItem->getQuantity() >= $config['qty'])
+                    {
+                        $adjustment = $adjustmentRepository->createNew();
+                        $adjustment->setAmount(- $orderItem->getTotal() * $configuration['percentage']);
+                        $adjustment->setLabel(OrderInterface::PROMOTION_ADJUSTMENT);
+                        $adjustment->setDescription($promotion->getDescription());
+
+                        return $adjustment;
+                    }
+                }
+            }
+
+            return null;
+        };
+
+        // DEBUG ---------- (stores all the Adjustments applied for promotions of the new product type)
+        $adjustments = [];
+        // ----------------
+
+        // the body of our hack:
+        // if promotion is of a product type,
+        // then check all order items and apply Adjustments where necessarily
+        if ($isProductPromotion($promotion)) {
+
+            // this additional IF to process a special case clearly indicates a lack of polymorphism in this code,
+            // the proper solution would be to turn an OrderItem to PromotionSubject and process it the same way as
+            // Order is processed, but this may be faster to get, especially if the rest of the code expects
+            // OrderInterface everywhere PromotionInterface is used
+
+            foreach ($subject->getItems() as $orderItem) {
+                if ($adjustment = $getProductPromotionAdjustment($orderItem)) {
+                    $subject->addAdjustment($adjustment);
+                    $adjustments[] = $adjustment;
+                }
+            }
+        }
+
+        // DEBUG ------------------------------------------- (dump Adjustments created by product promotions)
+        echo "Product adjustments applied:\n\n\n";
+        \Doctrine\Common\Util\Debug::dump($adjustments);die;
+        // -------------------------------------------------
+
+        // Product promo -----------------------------------------------------------------------------------------------
+
         $adjustment = $this->repository->createNew();
 
         $adjustment->setAmount(- $subject->getPromotionSubjectItemTotal() * ($configuration['percentage']));
